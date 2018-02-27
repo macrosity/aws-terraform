ff85a7af9642c8bd6353c54a74eb4907a7048331
Ste Rose <macrosity@gmail.com>
2018-02-27 22:11:01 +0000

    Add security group

--- main.tf
+++ main.tf
@@ -23,11 +23,29 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
     Name = "macro-terra"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "web-server-any"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
