6093fb4fabe801f834dd86fb3ce4c4fbc8c10946
Steve Rose <steven.rose@sky.uk>
2018-04-13 15:10:19 +0100

    Declare HTTP port as variable for repeatability

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "http_port" {
+  description = "HTTP Server default port"
+  default = 8080
+}
+
 provider "aws" {
   region = "${var.aws_region}"
 }
@@ -22,15 +27,15 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "nitro" {
   ami                    = "${data.aws_ami.ubuntu.id}"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = ["${aws_security_group.web_acl.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "{var.http_port}" &
               EOF
 
   tags {
@@ -38,12 +43,12 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "web_acl" {
   name = "web-server-any"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.http_port}"
+    to_port     = "${var.http_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
