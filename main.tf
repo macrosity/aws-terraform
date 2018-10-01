c458bf5dd9017b005929037367d88eb9af1e33e9
Steve Rose <steven.rose@sky.uk>
2018-10-01 16:18:29 +0100

    Latest changes

--- main.tf
+++ main.tf
@@ -1,38 +1,36 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "http_port" {
-  description = "HTTP Server default port"
-  default = 8080
-}
-
+# Providers for AWS Infrastructure go here
 provider "aws" {
-  region = "${var.aws_region}"
+  region = "eu-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = "true"
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+# Variables for AWS Infrastructure go here
+variable "http_port" {
+  description = "Standard HTTP server port"
+  default     = 8080
 }
 
+variable "sshd_port" {
+  description = "Standard SSH server port"
+  default     = 22
+}
+
+# Data outputs for AWS Infrastructure go here
 data "aws_availability_zones" "all" {}
 
-resource "aws_elb" "nitro_elb" {
-  name               = "aws-elb-nitro-cluster"
+# Resources for AWS Infrastructure go here
+resource "aws_key_pair" "macrosity-aws-key" {
+  key_name = "macrosity-aws-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+sVRDvgB3d/lMkBMB3FZnFI/XcvzSs9KMwJ1hRBcKVyqdQC/saaULIa4n8T8+V8A4LDLE5PCIAGoaoDBk6x8yZbxo8ec1BcauDO2khRIeGc8UxatlaznHbsUm43V4cM651qjdLfEmGk1ES8LXeWRx2+jsEm8+AR/oC6TW9cnplKJDRdMOopruq+SB5FyS8xEexc1R+WEHVs6jmzSKzpzspFVT1YRr6wlUv+/EdRXww2JiiCbrs/T/vzesIhExigLZk4E2JB/RG5bhvYY5KmHrZvou1RMD13bJXnzxy6+0/ToD4uBr2mlrRvVMXGc676SieQmFgwp1z5482Afq8tLJZKKzdiWL1QKN97SvEVvKyRcHti6qf3e1IZHq0i7ympc76iv1cpWPAiDPNrE2WGK+X84NTpATKY0TO71h4Ra3g6xmc+4b/N9y5uAIaq5HYsE1bFmsDbES5I47tRMUJEfkgoI+plR+YJt7rKp2f5EIZVyJ53R7EFSRKoU3t2FEpJWNVqn0Yfy6ZkjSwIHUkllasF962vjOujYQXhYerQPO47YmAv0ZVS70I+56+wkKDv7gJAdEoya0W3EGIIGWHMLqLi9Ts5jYF/PS6KASfhcfxnDUTd/TlsjtJ+xG8PujrCsMO4hs48XJPBQvAZ6wKtDYLYDK1ATfjDiPB7BuBQ8nIQ== sro75@MacBook"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_elb" "nitro-elb" {
+  name               = "nitro-elb-01"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
-  security_groups    = ["{$aws_security_group.elb_acl.id}"]
+  security_groups    = ["${aws_security_group.http-elb-default.id}"]
 
   listener {
     lb_port           = 80
@@ -50,67 +48,41 @@ resource "aws_elb" "nitro_elb" {
   }
 }
 
-resource "aws_security_group" "elb_acl" {
-  name = "elb-any"
+resource "aws_launch_configuration" "nitro-lc" {
+  image_id               = "ami-506e8f37"
+  instance_type          = "t2.micro"
+  key_name               = "macrosity-aws-key"
+  security_groups        = ["${aws_security_group.http-default-in.id}","${aws_security_group.sshd-default-in.id}"]
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  echo "Hello, World" > index.html
+                  nohup busybox httpd -f -p "${var.http_port}" &
+                  EOF
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "nitro" {
-  launch_configuration = "$aws_launch_configuration.nitro.id"
+resource "aws_autoscaling_group" "nitro-asc" {
+  launch_configuration = "${aws_launch_configuration.nitro-lc.id}"
   availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
-  load_balancers       = ["${data.aws_elb.nitro_elb.name}"]
-  health_check_type    = "ELB"
+  load_balancers    = ["${aws_elb.nitro-elb.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
-  max_size = 8
+  max_size = 5
 
   tag {
     key                 = "Name"
-    value               = "Nitro Auto-Scale Group"
-    propagate_at_launch = true
+    value               = "nitro-asc"
+    propagate_at_launch = "true"
   }
 }
 
-resource "aws_launch_configuration" "nitro" {
-  ami                    = "${data.aws_ami.ubuntu.id}"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.web_acl.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "{var.http_port}" &
-              EOF
-
-  lifecycle {
-    create_before_destroy = true # Always create resource before destroying original
-  }
-
-  tags {
-    Name = "macro-terra"
-  }
-}
-
-resource "aws_security_group" "web_acl" {
-  name = "web-any"
+resource "aws_security_group" "http-default-in" {
+  name          = "http-default-in"
 
   ingress {
     from_port   = "${var.http_port}"
@@ -124,10 +96,40 @@ resource "aws_security_group" "web_acl" {
   }
 }
 
-output "image_id" {
-  value = "${data.aws_ami.ubuntu.id}"
+resource "aws_security_group" "sshd-default-in" {
+  name          = "sshd-default-in"
+
+  ingress {
+    from_port   = "${var.sshd_port}"
+    to_port     = "${var.sshd_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+resource "aws_security_group" "http-elb-default" {
+  name = "http-elb-default"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Outputs for AWS Infrastructure go here
 output "elb_dns_name" {
-  value = "${aws_elb.nitro_elb.dns_name}"
+  value = "${aws_elb.nitro-elb.dns_name}"
 }
